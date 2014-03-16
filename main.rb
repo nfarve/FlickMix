e270069e2fd8478354da537e7e02c39ead9ad0e2
--replace-all <nfarve@mit.edu>
2014-03-16 13:38:41 -0400

    changes not sure what they are

--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
 # main.rb
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments'
+require './models/model'
 
 
   get '/' do
---
+++ models/model.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+require 'bcrypt'
+
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db.sqlite")
+
+class User
+  include DataMapper::Resource
+  include BCrypt
+
+  property :id, Serial, :key => true
+  property :username, String, :length => 3..50
+  property :password, BCryptHash
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
